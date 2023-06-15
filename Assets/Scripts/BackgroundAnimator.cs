7dfb10f17aad12df651c7e7517da75c0b9b6ba0f
NakXay KorYme <corby.maxime@gmail.com>
2023-06-15 12:34:34 +0200

    [FIX] : Background animations

--- Assets/Scripts/BackgroundAnimator.cs
+++ Assets/Scripts/BackgroundAnimator.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class BackgroundAnimator : MonoBehaviour
 {
     [Header("Parameters")]
-    [SerializeField, MinMaxSlider(0f, 10f)] Vector2 _animationTimeInBetween;
+    [SerializeField, MinMaxSlider(0f, 15f)] Vector2 _animationTimeInBetween;
 
     [Header("References")]
     [SerializeField] int _animationsNumber;
@@ -17,6 +17,7 @@ public class BackgroundAnimator : MonoBehaviour
     public void PlayAnimation()
     {
         if (_animationCoroutine != null) return;
+        Debug.Log("HEY");
         _animationCoroutine = StartCoroutine(AnimationCoroutine());
     }
 
@@ -24,5 +25,6 @@ public class BackgroundAnimator : MonoBehaviour
     {
         yield return new WaitForSeconds(Random.Range(_animationTimeInBetween.x, _animationTimeInBetween.y));
         _animator.SetTrigger("Trigger_" + Random.Range(0, _animationsNumber));
+        _animationCoroutine = null;
     }
 }
