8147e6ce256a02213a01c00607e5c93e740c9c6b
NakXay KorYme <corby.maxime@gmail.com>
2023-06-19 11:47:42 +0200

    [TEST] : Main Menu modifications

--- Assets/Scripts/PromotionCode.cs
+++ Assets/Scripts/PromotionCode.cs
@@ -12,41 +12,17 @@ public class PromotionCode : MonoBehaviour
     [Header("References")]
     [SerializeField] TMP_InputField inputField;
 
-    string tmpValue = "";
-
-    public void ValueChange(string str)
+    public void PromotionCodeCheck()
     {
-        if ((str == "" || str ==null) && tmpValue.Length > 1)
-        {
-            str = tmpValue;
-        }
-        else
-        {
-            tmpValue = str;
-        }
-        PromotionCodeCheck(tmpValue);
-    }
-
-    public void EndEdit(string str)
-    {
-        if (str == "" && tmpValue != "")
-        {
-            str = tmpValue;
-        }
-        PromotionCodeCheck(str);
-    }
-
-    public void PromotionCodeCheck(string str)
-    {
-        switch (str.ToUpper())
+        switch (inputField.text.ToUpper())
         {
             case "PULV":
-                Debug.Log(str.ToUpper());
+                Debug.Log(inputField.text.ToUpper());
                 DataManager.Instance.UnlockNewSkin(SKINPACK.PULV);
                 inputField.text = "";
                 break;
             case "ALLSTARS":
-                Debug.Log(str.ToUpper());
+                Debug.Log(inputField.text.ToUpper());
                 for (int i = 1; i < 51; i++)
                 {
                     DataManager.Instance.CompleteALevel(i, 4);
@@ -58,7 +34,7 @@ public class PromotionCode : MonoBehaviour
                 inputField.text = "";
                 break;
             case "ALLSKINS":
-                Debug.Log(str.ToUpper());
+                Debug.Log(inputField.text.ToUpper());
                 for (int i = 0; i < 7; i++)
                 {
                     DataManager.Instance.UnlockNewSkin((SKINPACK)i);
@@ -66,7 +42,7 @@ public class PromotionCode : MonoBehaviour
                 inputField.text = "";
                 break;
             case "RESET":
-                Debug.Log(str.ToUpper());
+                Debug.Log(inputField.text.ToUpper());
                 DataManager.Instance.InitializeData();
                 inputField.text = "";
                 Application.Quit();
